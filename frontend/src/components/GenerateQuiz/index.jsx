95807151e6ffb0307dddd0cc3c0770ebfd9231e4
Saikumarveeraboina <saikumar96030433@gmail.com>
2026-01-07 21:23:41 +0530

    files added

---
+++ frontend/src/components/GenerateQuiz/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+const GenerateQuiz = () => {
+  return (
+    <div>GenerateQuiz</div>
+  )
+}
+
+export default GenerateQuiz
---
+++ frontend/src/components/QuizModal/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+const QuizModal = () => {
+  return (
+    <div>QuizModal</div>
+  )
+}
+
+export default QuizModal
