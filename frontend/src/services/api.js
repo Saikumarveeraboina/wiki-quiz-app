bae7fcff4fcebdb84a3a15171e5518edf1358df0
Saikumarveeraboina <saikumar96030433@gmail.com>
2026-01-08 16:35:26 +0530

    updated fronted

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,15 @@
 const API_BASE = import.meta.env.VITE_API_BASE;
 
-
 export const generateQuiz = async (url) => {
   const res = await fetch(`${API_BASE}/generate-quiz`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ url })
+    body: JSON.stringify({ url }),
   });
-
-  return res.json();
+  return await res.json();
 };
 
 export const getPastQuizzes = async () => {
   const res = await fetch(`${API_BASE}/past-quizzes`);
-  return res.json();
+  return await res.json();
 };
